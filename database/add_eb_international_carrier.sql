4e1bf6657017de4222a129ced4d11216658e4f02
Felipe Cuartas <cuatrufel@gmail.com>
2026-08-08 15:23:55 +0200

    Add functional WhatsApp chat button to Processing Plants profile

---
+++ database/add_eb_international_carrier.sql
@@ -0,0 +1,2 @@
+insert into freight_carriers (name, contact_name, city, country, phone)
+values ('EB International Logistics LLC', 'Juan Carlos Barron', 'Hidalgo, TX', 'United States', '+1 (956) 648-5314');
